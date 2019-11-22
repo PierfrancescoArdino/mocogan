e468508287299ede5f7bff430f5389f0a36932c5
Pierfrancesco Ardino <pierfrancesco.ardino@studenti.unitn.it>
2019-11-22 14:55:43 +0100

    ** added files ** added motion encoder **first version cyclegan generator

---
+++ src/motion_encoder.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MotionEncoder(nn.Module):
+    def __init__(self):
+        super(MotionEncoder, self).__init__()
+
+        self.fc0 = nn.Linear(4096, 2048, bias=True)
+        self.fc1 = nn.Linear(2048, 1024, bias=True)
+        self.fc2 = nn.Linear(1024, 512, bias=True)
+        self.fc3 = nn.Linear(512, 256, bias=True)
+
+        self.fc4 = nn.Linear(256, 2, bias=True)
+        self.fc5 = nn.Linear(256, 2, bias=True)
+
+    def forward(self, x):
+        x = x.view(-1, 4096)
+        x = F.leaky_relu(self.fc0(x))
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
+        h1 = F.leaky_relu(self.fc3(x))
+        return self.fc4(h1), self.fc5(h1)
